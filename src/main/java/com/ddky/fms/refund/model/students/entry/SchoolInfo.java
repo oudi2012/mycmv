38c6c34b8c94168652baf3b137938dee0bfec4ee
oudi2012 <wanghaikuo000@163.com>
2020-06-24 15:07:30 +0800

    init_01_家庭作业和学校

---
+++ src/main/java/com/ddky/fms/refund/model/students/entry/SchoolInfo.java
@@ -0,0 +1,16 @@
+package com.ddky.fms.refund.model.students.entry;
+
+import lombok.Data;
+
+/***
+ * 学校信息
+ * @author a
+ */
+@Data
+public class SchoolInfo {
+
+    private Long        id;
+    private String      name;
+    private String      shortName;
+
+}
---
+++ src/main/java/com/ddky/fms/refund/model/userlogs/entry/HomeWork.java
@@ -0,0 +1,15 @@
+package com.ddky.fms.refund.model.userlogs.entry;
+
+import lombok.Data;
+
+/***
+ * 家庭作业
+ * @author a
+ */
+@Data
+public class HomeWork {
+
+    private Long        id;
+    private Long        userId;
+
+}
