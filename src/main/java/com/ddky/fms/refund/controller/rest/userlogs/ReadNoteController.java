bee1c0ebca25114bbe8d311b4d3cf40395b45ba2
oudi2012 <wanghaikuo000@163.com>
2020-07-16 19:27:52 +0800

    oper_logs_02

---
+++ src/main/java/com/ddky/fms/refund/controller/rest/userlogs/ReadNoteController.java
@@ -0,0 +1,48 @@
+package com.ddky.fms.refund.controller.rest.userlogs;
+
+
+import com.ddky.fms.refund.model.ResponseObject;
+import com.ddky.fms.refund.model.userlogs.entry.ReadNote;
+import com.ddky.fms.refund.service.ReadNoteService;
+import com.ddky.fms.refund.utils.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/***
+ *  userLogs ReadNote
+ * @author a
+ */
+@RestController
+@RequestMapping("userLogs/readNote")
+public class ReadNoteController {
+
+    @Autowired
+    private ReadNoteService opLogService;
+
+    @ResponseBody
+    @GetMapping("list")
+    public ResponseObject list(ReadNote termInfo, int pageIndex, int pageSize) {
+        ResponseObject resObj = new ResponseObject();
+        CommonUtils.executeSuccess(resObj, opLogService.list(termInfo, pageIndex, pageSize));
+        return resObj;
+    }
+
+    @ResponseBody
+    @PostMapping("create")
+    public ResponseObject create(@RequestBody ReadNote termInfo) {
+        ResponseObject resObj = new ResponseObject();
+        opLogService.insert(termInfo);
+        CommonUtils.executeSuccess(resObj, termInfo);
+        return resObj;
+    }
+
+    @ResponseBody
+    @GetMapping("findById")
+    public ResponseObject findById(Integer id) {
+        ResponseObject resObj = new ResponseObject();
+        ReadNote termInfo = opLogService.findById(id);
+        CommonUtils.executeSuccess(resObj, termInfo);
+        return resObj;
+    }
+
+}
---
+++ src/main/java/com/ddky/fms/refund/controller/rest/userlogs/StudentWorkController.java
@@ -0,0 +1,48 @@
+package com.ddky.fms.refund.controller.rest.userlogs;
+
+
+import com.ddky.fms.refund.model.ResponseObject;
+import com.ddky.fms.refund.model.userlogs.entry.StudentWork;
+import com.ddky.fms.refund.service.StudentWorkService;
+import com.ddky.fms.refund.utils.CommonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/***
+ *  userLogs StudentWork
+ * @author a
+ */
+@RestController
+@RequestMapping("userLogs/studentWork")
+public class StudentWorkController {
+
+    @Autowired
+    private StudentWorkService opLogService;
+
+    @ResponseBody
+    @GetMapping("list")
+    public ResponseObject list(StudentWork termInfo, int pageIndex, int pageSize) {
+        ResponseObject resObj = new ResponseObject();
+        CommonUtils.executeSuccess(resObj, opLogService.list(termInfo, pageIndex, pageSize));
+        return resObj;
+    }
+
+    @ResponseBody
+    @PostMapping("create")
+    public ResponseObject create(@RequestBody StudentWork termInfo) {
+        ResponseObject resObj = new ResponseObject();
+        opLogService.insert(termInfo);
+        CommonUtils.executeSuccess(resObj, termInfo);
+        return resObj;
+    }
+
+    @ResponseBody
+    @GetMapping("findById")
+    public ResponseObject findById(Integer id) {
+        ResponseObject resObj = new ResponseObject();
+        StudentWork termInfo = opLogService.findById(id);
+        CommonUtils.executeSuccess(resObj, termInfo);
+        return resObj;
+    }
+
+}
