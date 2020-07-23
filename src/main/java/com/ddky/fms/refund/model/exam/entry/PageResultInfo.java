7183d7858e74bcba2e3fb6d9947452c4a9ad05e7
oudi2012 <wanghaikuo000@163.com>
2020-07-23 11:01:47 +0800

    exam_update_02

---
+++ src/main/java/com/ddky/fms/refund/model/exam/entry/PageResultInfo.java
@@ -0,0 +1,21 @@
+package com.ddky.fms.refund.model.exam.entry;
+
+
+import lombok.Data;
+
+/***
+ * @author a
+ * id, examPageId, userId, questionId, answers, isRight, score, state, createTime
+ */
+@Data
+public class PageResultInfo {
+    private Integer id;
+    private Integer examPageId;
+    private Long userId;
+    private Integer questionId;
+    private String answers;
+    private Integer isRight;
+    private Integer score;
+    private Integer state;
+    private Integer createTime;
+}
